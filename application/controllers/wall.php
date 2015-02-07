d873f465ce33e3344d40501ef9b3e24575d2d2ff
Sony Surahman <sony.surahman98@gmail.com>
2015-02-08 01:10:15 +0700

    Ubah routes sama controller

--- application/controllers/wall.php
+++ application/controllers/wall.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Wall extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		echo "<center><h1>Welcome to WallSite</h1></center>";
 	}
 }
 
